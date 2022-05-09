9cd95dada578500428c385887da13c2a6f1fefef
Alejandro <alejandrom.am33@gmail.com>
2022-05-09 14:10:32 -0500

    add: router para partners

--- src/router.js
+++ src/router.js
@@ -7,14 +7,29 @@ const routes =  [
     component: () => import("./components/ExplorersList")
   },
   {
+    path: "/partner",
+    name: "partner",
+    component: () => import("./components/PartnerList")
+  },
+  {
     path: "/explorer/:id",
     name: "explorer-details",
     component: () => import("./components/Explorer")
   },
   {
+    path: "/partner/:id",
+    name: "partner-details",
+    component: () => import("./components/Partner")
+  },
+  {
     path: "/add",
     name: "add-explorer",
     component: () => import("./components/AddExplorer")
+  },
+  {
+    path: "/addpartner",
+    name: "add-partner",
+    component: () => import("./components/AddPartner")
   }
 ];
 const router = createRouter({
